334351fdef2483f3a29d8308081ce9c48316314f
cylim <pika8976@naver.com>
2019-10-17 16:21:00 +0900

    localStorage에 favorite 정보 저장 구현

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,35 +1,80 @@
-import React, {useState} from 'react';
-import './Card.css';
+import React, { useState, useEffect } from "react";
+import "./Card.css";
 
-const onImg = '/img/on-img/on-img.png';
-const onImg2 = '/img/on-img/on-img@2x.png';
-const onImg3 = '/img/on-img/on-img@3x.png';
-const blue = '/img/blue/blue.png';
-const blue2 = '/img/blue/blue@2x.png';
-const blue3 = '/img/blue/blue@3x.png';
+const onImg = "/img/on-img/on-img.png";
+const onImg2 = "/img/on-img/on-img@2x.png";
+const onImg3 = "/img/on-img/on-img@3x.png";
+const blue = "/img/blue/blue.png";
+const blue2 = "/img/blue/blue@2x.png";
+const blue3 = "/img/blue/blue@3x.png";
 
-const Card = ({id, image_url, nickname, profile_image_url}) => {
-    const [Checked, setChecked] = useState(false);
+const Card = ({ id, image_url, nickname, profile_image_url }) => {
+  const [Checked, setChecked] = useState(false);
 
-    const onClickEvt = () => {
-        let checkedState = Checked;
-        setChecked(!checkedState);
+  useEffect(() => {
+    let ids = localStorage.getItem("favorite");
+    let arr = [];
+    console.log(ids);
+    if (ids) {
+      arr = JSON.parse(ids);
+      if (arr.includes(id)) {
+        setChecked(true);
+      }
     }
+  }, []);
 
-    return (
-        <div className="card_container">
-            <div className="top_container">
-                <img src={profile_image_url} className="ic_avatar_cat"/>
-                <div className="Tommy-Cummings">{nickname} </div>
+  const onClickEvt = () => {
+    let checkedState = Checked;
 
-            </div>
-            <div className="ic_favorite">
-                <img src={image_url} className="Rectangle"/>
-                {Checked?<img src={blue} srcSet={`${blue2} 2x, ${blue3} 3x`} className="on-img" onClick={onClickEvt}/>:
-                <img src={onImg} srcSet={`${onImg2} 2x, ${onImg3} 3x`} className="on-img" onClick={onClickEvt}/>}
-            </div>
-        </div>
-    );
-}
+    setChecked(!checkedState);
+
+    let ids = localStorage.getItem("favorite");
+    let arr = [];
+    if (!Checked) {
+      if (ids) {
+        arr = JSON.parse(ids);
+        arr.push(id);
+        arr = Array.from(new Set(arr));
+        localStorage.setItem("favorite", JSON.stringify(arr));
+      } else {
+        arr = [];
+        arr.push(id);
+        localStorage.setItem("favorite", JSON.stringify(arr));
+      }
+    } else {
+      arr = JSON.parse(ids);
+      arr = arr.filter(id_ => id_ !== id);
+      arr = Array.from(new Set(arr));
+      localStorage.setItem("favorite", JSON.stringify(arr));
+    }
+  };
+
+  return (
+    <div className="card_container">
+      <div className="top_container">
+        <img src={profile_image_url} className="ic_avatar_cat" />
+        <div className="Tommy-Cummings">{nickname} </div>
+      </div>
+      <div className="ic_favorite">
+        <img src={image_url} className="Rectangle" />
+        {Checked ? (
+          <img
+            src={blue}
+            srcSet={`${blue2} 2x, ${blue3} 3x`}
+            className="on-img"
+            onClick={onClickEvt}
+          />
+        ) : (
+          <img
+            src={onImg}
+            srcSet={`${onImg2} 2x, ${onImg3} 3x`}
+            className="on-img"
+            onClick={onClickEvt}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default Card;
